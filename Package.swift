38b8adf349104f1761e69fed0904f045ae8e412d
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-07-30 16:37:16 -0700

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "iGetGroceriesGroceryItems",
+    platforms: [
+        .iOS(.v17), .macOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "iGetGroceriesGroceryItems",
+            targets: ["iGetGroceriesGroceryItems"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/iGetGroceries/iGetGroceriesSharedUI.git", branch: "main")
+    ],
+    targets: [
+        .target(
+            name: "iGetGroceriesGroceryItems",
+            dependencies: [
+                "iGetGroceriesSharedUI"
+            ]
+        ),
+        .testTarget(
+            name: "iGetGroceriesGroceryItemsTests",
+            dependencies: ["iGetGroceriesGroceryItems"]
+        ),
+    ]
+)
---
+++ Sources/iGetGroceriesGroceryItems/Public/Main/GroceriesMainView.swift
@@ -0,0 +1,21 @@
+//
+//  GroceriesMainView.swift
+//  
+//
+//  Created by Nikolai Nobadi on 7/30/24.
+//
+
+import SwiftUI
+import iGetGroceriesSharedUI
+
+struct GroceriesMainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+// MARK: - Preview
+#Preview {
+    GroceriesMainView()
+}
